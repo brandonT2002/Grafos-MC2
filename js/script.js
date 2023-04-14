5e5a74b250103aa2b4f05cad1657915f6e3f1613
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-04-13 22:24:27 -0600

    se implementa el algoritmo de busqueda de caminos

--- js/script.js
+++ js/script.js
@@ -36,10 +36,16 @@ function analizar(){
 }
 function analizarGrafo(vIni,vFin,grafo) {
     console.log('NUEVO PARSEO')
+    // console.log(grafo)
     if(grafo[vIni] != null && grafo[vFin] != null) {
-        Object.entries(grafo).forEach(([vertice,enlaces]) => {
-            console.log(`${vertice}: ${enlaces}`);
-        });
+        // Object.entries(grafo).forEach(([vertice,enlaces]) => {
+        //     console.log(`${vertice}: ${enlaces}`);
+        // });
+
+        // llamar a la función buscar caminos
+        caminos = buscarCaminos(grafo,vIni,vFin);
+        caminos.sort((a,b) => a.length - b.length)
+        console.log(caminos)
         return;
     }
     swal({
@@ -47,6 +53,27 @@ function analizarGrafo(vIni,vFin,grafo) {
         text: "Verifique los vertices",
         icon: "info"
     })
+}
+function buscarCaminos(grafo,vIni,vFin,visitados = []){
+    let caminos = [];
+    visitados.push(vIni);
+
+    if (vIni === vFin){
+        return [visitados]
+    }
+    if (!grafo[vIni]){
+        return [];
+    }
+    
+    for (let vecino of grafo[vIni]){
+        if (!visitados.includes(vecino)){
+            let subCaminos = buscarCaminos(grafo, vecino, vFin, visitados.slice())
+            for (let subCamino of subCaminos){
+                caminos.push(subCamino);
+            }
+        }
+    }
+    return caminos;
 }
 function limpiarCampos(){
     document.getElementById('vIni').value = ''
